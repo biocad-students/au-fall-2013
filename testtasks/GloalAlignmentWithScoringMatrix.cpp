3d40a09edf2d22fdcbe9519cabb706f04b4590ea
1dayac <1dayac@rambler.ru>
2013-09-30 14:34:35 +0400

    Now it can read any symmetric matrix

--- testtasks/GloalAlignmentWithScoringMatrix.cpp
+++ testtasks/GloalAlignmentWithScoringMatrix.cpp
@@ -3,45 +3,63 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
-using std::pair;
-using std::cin;
-using std::cout;
-using std::string;
-using std::endl;
-using std::max;
-using std::vector;
-using std::map;
-using std::make_pair;
-int mas[1010][1010] = {};
-map<pair<char, char>, int> blossum;
+#include <iterator>
+#include <algorithm>
+	
+	std::vector<char> &split(const std::string &s, char delim, std::vector<char> &elems) 
+	{
+		std::stringstream ss(s);
+		std::string item;
+		while (std::getline(ss, item, delim)) 
+		{
+			if(item[0]!='\0')
+			{
+				elems.push_back(item[0]);
+			}
+		}
+			return elems;
+	}
 
-string supmas = "ACDEFGHIKLMNPQRSTVWY";
+
+	std::vector<char> split(const std::string &s, char delim) 
+	{
+		std::vector<char> elems;
+		split(s, delim, elems);
+		return elems;
+	}
 
 	int main()
 	{
+		std::map<std::pair<char, char>, int> blossum;
+		std::ifstream matrix("matrix.txt");
 		std::ifstream file("input.txt");
 		std::ofstream outfile("output.txt");
 		std::string str;
-		std::getline( file, str );
-
-		for(int i = 0; i<20; i++)
+		do
+		{
+			std::getline(matrix, str);
+		}
+		while(str[0]=='#');
+		std::vector<char> supmas = split(str, ' '); 
+		for(int i = 0; i<supmas.size(); ++i)
 		{
 			char ch;
-			file>>ch;
-			for(int j = 0; j<20; j++)
+			matrix>>ch;
+			for(int j = 0; j<supmas.size(); ++j)
 			{
 				int z;
-				file>>z;
-				blossum[make_pair(supmas[i], supmas[j])]=z;
+				matrix>>z;
+				blossum[std::make_pair(supmas[i], supmas[j])]=z;
 			}
 		}
 
-		string a;
-		string b;
-		string tmp;
+		std::string a;
+		std::string b;
+		std::string tmp;
 		file>>tmp;
 
 		while(file>>tmp)
@@ -56,58 +74,59 @@ string supmas = "ACDEFGHIKLMNPQRSTVWY";
 			b+=tmp;
 		}
 
+		std::vector<std::vector<int>> mas(std::max(a.length(), b.length())+1, std::vector<int>(std::max(a.length(), b.length())+1, 0));
 		int gaptax = -5;
-		for(int i = 1; i <=max(a.length(), b.length()); i++)
+		for(int i = 1; i <= std::max(a.length(), b.length()); ++i)
 		{
 			mas[0][i]=gaptax*i;
 			mas[i][0]=gaptax*i;
 		}
 
-		for(int i = 1; i <=a.length(); i++)
+		for(int i = 1; i <=a.length(); ++i)
 		{
-			for(int j =1; j<=b.length(); j++)
+			for(int j =1; j<=b.length(); ++j)
 			{
-				mas[i][j]=max(mas[i-1][j]+gaptax, max(mas[i][j-1]+gaptax, mas[i-1][j-1]+blossum[make_pair(a[i-1], b[j-1])]));
+				mas[i][j]= std::max(mas[i-1][j]+gaptax, std::max(mas[i][j-1]+gaptax, mas[i-1][j-1]+blossum[std::make_pair(a[i-1], b[j-1])]));
 			}
 		}
 		
-		vector<char> answer(0);
-		vector<char> answer2(0);
+		std::vector<char> answer(0);
+		std::vector<char> answer2(0);
 		int indexx = a.length();
 		int indexy = b.length();
 		while(indexx!=0&&indexy!=0)
 		{
-			if(indexy!=0&&mas[indexx][indexy]==mas[indexx][indexy-1]+gaptax)
+			if(indexy != 0 && mas[indexx][indexy] == mas[indexx][indexy - 1]+gaptax)
 			{
 				answer2.push_back(b[indexy-1]);
 				answer.push_back('-');
 				indexy--;
 			}
 			else
-			if(indexx!=0&&mas[indexx][indexy]==mas[indexx-1][indexy]+gaptax)
+			if(indexx != 0 && mas[indexx][indexy] == mas[indexx - 1][indexy] + gaptax)
 			{
 				answer.push_back(a[indexx-1]);
 				answer2.push_back('-');
 				indexx--;
 			}
 			else
-				if(mas[indexx][indexy]==mas[indexx-1][indexy-1]+blossum[make_pair(a[indexx-1], b[indexy-1])])
-			{
+				if(mas[indexx][indexy] == mas[indexx - 1][indexy - 1] + blossum[std::make_pair(a[indexx - 1], b[indexy - 1])])
+			{ 
 				answer.push_back(a[indexx-1]);
 				answer2.push_back(b[indexy-1]);
 				indexx--;
 				indexy--;
 			}
 		}
-		cout<<mas[a.length()][b.length()]<<endl;
+		std::cout<<mas[a.length()][b.length()]<<std::endl;
 		for(int i = answer.size(); i>0; --i)
 		{
-			cout<<answer[i-1];
+			std::cout<<answer[i-1];
 		}
-		cout<<endl;
+		std::cout<<std::endl;
 		for(int i = answer2.size(); i>0; --i)
 		{
-			cout<<answer2[i-1];
+			std::cout<<answer2[i-1];
 		}
 
 		return 0;
